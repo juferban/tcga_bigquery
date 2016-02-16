f5caadbfaf10fca3b2f38f739ff6182d21e2b4e4
Julio Fernandez <julio.fernandez@gmail.com>
2016-02-16 12:11:59 -0800

    Added the correlation for the treated and untreated status. Now it returns a result to large to retrieve.

--- sql/prognosis_vs_gene_expression_treated.sql
+++ sql/prognosis_vs_gene_expression_treated.sql
@@ -8,7 +8,9 @@ SELECT
    t.y as y, 
    t.sy2 as sy2, 
    t.ny as ny, 
-   (p.x-t.y) / SQRT((p.sx2/p.nx) + (t.sy2/t.ny)) as T 
+   (p.x-t.y) / SQRT((p.sx2/p.nx) + (t.sy2/t.ny)) as T, 
+   p.pc as untreated_survival_correlation,
+   t.pc_t as treated_survival_correlation
 FROM ( 
   SELECT a_exp.target_gene, 
        a_exp.SampleTypeLetterCode, 
@@ -16,6 +18,7 @@ FROM (
        avg(log2_count) as x,
        pow(stddev(log2_count),2) as sx2,
        count(distinct(a_exp.SampleBarcode)) AS nx, 
+       corr(a_exp.log2_count, b_clin.days_to_last_known_alive) AS pc
   FROM (
     SELECT ParticipantBarcode,
            SampleBarcode, 
@@ -23,7 +26,7 @@ FROM (
            AliquotBarcode,
            Study                as cancer_type, 
            HGNC_gene_symbol     as target_gene, 
-           log2(normalized_count+1) As log2_count,         
+           log2(normalized_count+1) As log2_count 
     FROM   [isb-cgc:tcga_201510_alpha.mRNA_UNC_HiSeq_RSEM]
   ) as a_exp
   JOIN (
@@ -43,6 +46,7 @@ JOIN (
        avg(log2_count) as y,
        pow(stddev(log2_count),2) as sy2,
        count(distinct(a_exp.SampleBarcode)) AS ny, 
+       corr(a_exp.log2_count, b_clin.days_to_last_known_alive) AS pc_t
   FROM (
     SELECT ParticipantBarcode,
          SampleBarcode, 
@@ -50,7 +54,7 @@ JOIN (
          AliquotBarcode,
          Study                as cancer_type, 
          HGNC_gene_symbol     as target_gene, 
-         log2(normalized_count+1) As log2_count,         
+         log2(normalized_count+1) As log2_count  
     FROM   [isb-cgc:tcga_201510_alpha.mRNA_UNC_HiSeq_RSEM]
   ) as a_exp
   join (
@@ -65,5 +69,4 @@ JOIN (
 ) AS t 
 ON 
 p.target_gene = t.target_gene and p.cancer_type = t.cancer_type 
-GROUP BY gene, cancer_type, x, sx2, nx, y, sy2, ny, T, mean_diff 
-
+GROUP BY gene, cancer_type, x, sx2, nx, y, sy2, ny, T, mean_diff, untreated_survival_correlation, treated_survival_correlation 
